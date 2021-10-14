5b1b54f05f0bdeddbf483e3001f461a78194fa58
Ruan4S <ruands2002@gmail.com>
2021-10-14 02:24:52 -0300

    Criando arquivos, importando express/ejs e configuriniciando server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+//Dizendo para o Express usar o EJS como view engine
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(8080, () => console.log('Servidor iniciado'))
