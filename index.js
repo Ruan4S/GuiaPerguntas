3c465ad350b6e15f11bfe8bf758abcf54b2fc9cb
Ruan4S <ruands2002@gmail.com>
2021-10-14 02:42:37 -0300

    Inserindo variáveis do back no front

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const app = express();
 //Dizendo para o Express usar o EJS como view engine
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/:nome/:lang', (req, res) => {
+    let nome = req.params.nome;
+    let lang = req.params.lang;
+    res.render('index', {
+        nome: nome,
+        lang: lang,
+    });
 });
 
 app.listen(8080, () => console.log('Servidor iniciado'))
