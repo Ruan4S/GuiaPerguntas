1fc6e1d611ce3b9d3e5aeb80418fcab42f0ccd2b
Ruan4S <ruands2002@gmail.com>
2021-10-14 19:54:53 -0300

    Adicionando bootstrap

--- index.js
+++ index.js
@@ -5,26 +5,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/:nome/:lang', (req, res) => {
-    let nome = req.params.nome;
-    let lang = req.params.lang;
-    let exibirMsg = true;
-
-    var produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Coca-cola", preco: 5},
-        {nome: "Leite", preco: 1.45},
-        {nome: "Carne", preco: 15},
-        {nome: "RedBull", preco: 6},
-        {nome: "Nescau", preco: 4},
-    ]
-
-    res.render('index', {
-        nome: nome,
-        lang: lang,
-        msg: exibirMsg,
-        produtos: produtos,
-    });
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
 app.listen(8080, () => console.log('Servidor iniciado'))
