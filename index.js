db078136e4e1dbab28b9c1046f6fe13c251b5f72
Ruan4S <ruands2002@gmail.com>
2021-10-14 20:59:59 -0300

    Enviando dados para o backend

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 //Dizendo para o Express usar o EJS como view engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
+//Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+//Rotas
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -13,4 +17,10 @@ app.get('/perguntar', (req, res) => {
     res.render('perguntar');
 });
 
-app.listen(8080, () => console.log('Servidor iniciado'))
+app.post('/salvarpergunta', (req, res) => {
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
+    res.send(`${titulo} + ${descricao}`)
+});
+
+app.listen(8080, () => console.log('Servidor iniciado'));
