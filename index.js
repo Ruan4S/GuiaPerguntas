e75d37e1fb1c39ec7cb930d239b071695adc8ecd
Ruan4S <ruands2002@gmail.com>
2021-10-14 21:20:56 -0300

    Configurando conexão com bando de dados

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+//Importando conexão com banco de dados
+const connection = require('./database/database');
+
+//Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log('Conexão feita com o banco de dados!')
+    })
+    .catch((e) => {
+        console.log(e)
+    });
 
 //Dizendo para o Express usar o EJS como view engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 //Body parser
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 //Rotas
 app.get('/', (req, res) => {
